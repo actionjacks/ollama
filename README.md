14a408836cdd80be3f193134bd9f73a6b799b1ea
Jacek Zablocki <jzablocki@netis.pl>
2025-03-21 11:33:44 +0100

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+docker exec -it ollama-ollamadeepseek-1 sh
+
+docker logs ollama-ollamadeepseek-1
+
+ollama run llama3:8b "Hello, how are you?"
---
+++ ollama/entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+export OLLAMA_PORT=11436
+
+if [ -z "$(which nvidia-smi)" ]; then
+    echo "Brak dostępu do GPU"
+else
+    echo "Wykryto GPU"
+    export CUDA_VISIBLE_DEVICES=0
+fi
+
+echo "Starting Ollama..."
+ollama serve & # Start Ollama in the background
+
+sleep 5         # Wait for it to initialize
+
+echo "Downloading models..."
+# ollama pull deepseek-r1:14b
+ollama pull llama3:8b | tee /var/log/ollama_pull.log
+
+# If there is a startup file in the api directory, run it (e.g. Python)
+# if [ -f /app/api/main.py ]; then
+#     echo "Uruchamiam API..."
+#     python3 /app/api/main.py &
+# fi
+
+if [ -f "/app/context/doc.txt" ]; then
+    echo "Creating custom model based on doc.txt..."
+    ollama create netiskuba -f /app/context/doc.txt
+fi
+
+echo "Ollama is ready."
+tail -f /dev/null  # Keep the container running
